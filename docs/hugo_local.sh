829d2f4867f6733864a8e05c00fdd4c0dd9cc54e
Holger Pandel <holger.pandel@googlemail.com>
2024-05-11 15:50:09 +0200

    Docu update: restructure site, add comments

--- docs/hugo_local.sh
+++ docs/hugo_local.sh
@@ -3,7 +3,6 @@ cd themes
 git clone https://github.com/thingsym/hugo-theme-techdoc techdoc
 git clone https://github.com/mfg92/hugo-shortcode-gallery.git
 cd ..
-cp -a theme_mod/. themes/techdoc/
 
 hugo serve --noHTTPCache --disableFastRender --cleanDestinationDir
 rm -rf public
